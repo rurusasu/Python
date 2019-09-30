e19372de7110838d607330a80ceeffc610944c18
F116056 <F116056@ed.socu.ac.jp>
2019-09-30 19:24:16 +0900

    偏微分変更

--- AI/NeuralNet/Deep_Learning_from_screatch/ch_4ニューラルネットワークの学習/4.3.3偏微分.py
+++ AI/NeuralNet/Deep_Learning_from_screatch/ch_4ニューラルネットワークの学習/4.3.3偏微分.py
@@ -3,9 +3,54 @@
 import numpy as np
 import matplotlib as plt
 
+
+
+def _numerical_gradient_no_batch(f, x):
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)  # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)  # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+        x[idx] = tmp_val # 値を元に戻す
+
+    return grad
+
+
+
+def numerical_gradient(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_no_batch(f, x)
+    else:
+        grad = np.zeros_like(x) # gradの初期化
+
+        for idx, x in enumerate(X): # enumrate(x) インデックス番号, 要素の順に取得する
+            grad[idx] = _numerical_gradient_no_batch(f, x)
+
+        return grad
+
+
 def function_2(x):
-    return np.sum(x**2)
+    if x.ndim == 1:
+        return np.sum(x**2)
+    else:
+        return np.sum(x**2, axis=1)
 
 
-x = arrange(0.0, 20.0, 0.1)
-y = function_2(x)
+if __name__ == '__main__':
+    x0 = np.arange(-2, 2.5, 0.25)
+    x1 = np.arange(-2, 2.5, 0.25)
+    X, Y = np.meshgrid(x0, x1) # meshgrid：配列の要素から格子列を生成する
+
+    # flatten：配列を1次元に変換する
+    X = X.flatten() 
+    Y = Y.flatten()
+
+    grad = numerical_gradient(function_2, np.array[X, Y])
+    print(grad)
