8af66ddaa8ea5be0380c0290b6d993bf65188250
rurusasu <36875744+rurusasu@users.noreply.github.com>
2019-10-16 18:36:58 +0900

    pythonDemoにデータ整理の機能を追加

--- Robot/Python_Demo/アプリケーション_2.py
+++ Robot/Python_Demo/アプリケーション_2.py
@@ -30,6 +30,11 @@ hCheck   = [] # チェックボックスのハンドルを格納する
 CheckVal = [] # チェックボックスにチェックが入っているかを格納する
 
 
+def __filePath__(file_name):
+    path = './data/' + str(file_name)
+    return path
+
+
 #------------------------
 # ボタンが押されたときの処理
 #------------------------
@@ -81,7 +86,7 @@ def DetaGet_click():
     counter_y_init = -201
     counter_z = 101
 
-    file_name=txt.get()
+    file_name=__filePath__(txt.get())
     #-----------------------------
     # 以下Z軸方向の動作
     #-----------------------------
@@ -123,6 +128,23 @@ def DetaGet_click():
     pose = dType.GetPose(api)
 
 
+def DetaMake_click():
+    inputPath = __filePath__(txt.get())
+    learnPath = __filePath__(txt_2.get())
+    testPath = __filePath__(txt_3.get())
+    digit = str(10^int(txt_4.get()))
+
+    io = csvIO()
+    v = io.open_twoD_array(inputPath)
+    v = io.twoD_FroatToStr(v, digit=digit)
+    l_array = io.Get_AnytwoD_array(v, col_range_end=3)
+    t_array = io.Get_AnytwoD_array(v, col_range_first=4, col_range_end=7)
+
+    io.csv_write(learnPath, l_array)
+    io.csv_write(testPath, t_array)
+
+
+
 
 # ウインドウの作成
 win = tk.Tk()
@@ -135,7 +157,7 @@ win.geometry('500x250')  # サイズを指定
 #------------
 # テキストボックス用ラベル
 lbl = tk.Label(text='SaveFile')
-lbl.place(x=55, y=50)
+lbl.place(x=33, y=50)
 # データ変換部分用ラベル
 lbl_2 = tk.Label(text='DataConv')
 lbl_2.place(x=197, y=0)
@@ -145,6 +167,9 @@ lbl_3.place(x=170, y=20)
 # テスト用データラベル
 lbl_4 = tk.Label(text='test')
 lbl_4.place(x=170, y=60)
+# 小数点以下の桁数用ラベル
+lbl_5 = tk.Label(text='小数点以下の桁数')
+lbl_5.place(x=230, y=60)
 
 
 #------------
@@ -156,19 +181,27 @@ connectBtn.place(x=20, y=10)
 datagetBtn = tk.Button(win, text='DataGet', command=DetaGet_click, width=10)
 datagetBtn.place(x=20, y=100)
 
+datamakeBtn = tk.Button(win, text='DataMake', command=DetaMake_click, width=10)
+datamakeBtn.place(x=170, y=100)
+
 #---------------------
 # テキストボックス
 #---------------------
-txt = tk.Entry(width=20)
+txt = tk.Entry(width=13)
 txt.place(x=20, y=70)
-txt.insert(tk.END, './data/data.csv') # テキストボックスに文字をセット
+txt.insert(tk.END, 'data.csv') # テキストボックスに文字をセット
 # 学習用データ用
-txt_2 = tk.Entry(width=20)
+txt_2 = tk.Entry(width=13)
 txt_2.place(x=170, y=40)
 txt_2.insert(tk.END, 'learn.csv')
 # テスト用
-txt_3 = tk.Entry(width=20)
+txt_3 = tk.Entry(width=13)
 txt_3.place(x=170, y=80)
 txt_3.insert(tk.END, 'test.csv')
+# 小数点以下の桁数
+txt_4 = tk.Entry(width=3)
+txt_4.place(x=260, y=80)
+txt_4.insert(tk.END, '2')
+
 
 win.mainloop()          # ウインドウを動かす
