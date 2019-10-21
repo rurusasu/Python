e2fb55b662ea1941b8fb4c43ef90f26f6725496c
rurusasu <36875744+rurusasu@users.noreply.github.com>
2019-10-21 10:27:21 +0900

    レイヤのインポートミスを修正

--- AI/NeuralNet/nn_pt5/pt5.py
+++ AI/NeuralNet/nn_pt5/pt5.py
@@ -2,8 +2,8 @@ import sys, os
 sys.path.append(os.pardir)
 import numpy as np
 import matplotlib.pyplot as plt
-from nn_pt1.functions import *
-from nn_pt1.layers import *
+from nn_pt5.functions import *
+from nn_pt5.layers import *
 from collections import OrderedDict
 
 class output: pass
