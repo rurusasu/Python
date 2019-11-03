43ae38dc26500bf8ccdadbaca3c543d7688c55f3
rurusasu <36875744+rurusasu@users.noreply.github.com>
2019-11-03 11:32:37 +0900

    アプリケーションをDobot用とNeuralNet用に分割 アプリケーション_3：Dobot用 アプリケーション_4：NeuralNet用

--- Robot/Python_Demo/アプリケーション_3.py
+++ Robot/Python_Demo/アプリケーション_3.py
@@ -8,24 +8,11 @@ import PySimpleGUI as sg
 import numpy as np
 from Dobot import*
 from common.csvIO import dataConv
-from nn import NeuralNet
 
 
 def __filePath__(file_name):
     path = './data/' + str(file_name)
     return path
-
-def dataLoad(filePath, dtype):
-    if filePath != str:
-        filePath = str(filePath)
-    x = np.loadtxt(
-            filePath, #読み込むファイル名(例"save_data.csv")
-            dtype=dtype,     #データのtype
-            delimiter=",",   #区切り文字の指定
-            ndmin=2          #配列の最低次元
-        )
-    return x
-
 
 # ----- The callback function ----- #
 def SaveOriginal_click(CON_STR, file_name):
@@ -100,50 +87,6 @@ def SaveValidation_click(CON_STR, file_name):
 
     print('testデータ取得が終了しました。')
 
-def DataMake_click(org_FileName, lrn_FileName, tst_FileName, digit):
-    lrn_FileName = __filePath__(lrn_FileName)
-    tst_FileName = __filePath__(tst_FileName)
-    #dataConv(org_FileName, lrn_FileName, col_range_end=3, digit=digit)
-    #dataConv(org_FileName, tst_FileName, col_range_first=4, col_range_end=7, digit=digit)
-
-def NetMake_click(loss):
-    # コンストラクタ
-    nn = NeuralNet()
-
-    nn.netmake(loss)
-
-def Training_click(orgPath, batch_size, epochs, feature=None, valPath=None ):
-    orgLRN_Path = orgPath[0]
-    orgTrg_Path = orgPath[1]
-
-    #---------------
-    # 型変換
-    #---------------
-    if batch_size != int:
-        batch_size = int(batch_size)
-    if epochs != int:
-        epochs = int(epochs)
-
-
-    # データの読み込み
-    #訓練データの読み込み
-    x = dataLoad(orgLRN_Path, float)
-    t = dataLoad(orgTrg_Path, float)
-
-    # Validationデータ
-    if valPath != None:
-        valRLN_Path = valPath[0]
-        valTrg_Path = valPath[1]
-
-        x_val = dataLoad(valRLN_Path, float)
-        t_val = dataLoad(valTrg_Path, float)
-
-        validation = (x_val, t_val)
-    else:
-        validation = None
-
-    nn.nn(x, t, batch_size, epochs, feature, validation)
-
 
 # ----- Menu Definition ----- #
 menu_def = [['File', ['Open', 'Save', 'Exit', 'Properties']],
@@ -165,51 +108,6 @@ saveVal = [
     [sg.Button('SaveValidation', key='-SaveValidation-')],
 ]
 
-dataConv = [
-    [sg.Text('OrigiData')],
-    [sg.InputText('data.csv', size=(13, 1)), sg.FileBrowse(key='-orgData-')],
-    [sg.Text('LearnData')],
-    [sg.InputText('learn.csv', size=(13, 1), key='-lrnData-')],
-    [sg.Text('TestData'),
-     sg.Text('小数点以下の桁数')],
-    [sg.InputText('test.csv', size=(13, 1), key='-tstData-'),
-     sg.InputText('2', size=(5, 1), key='-Digit-')],
-    [sg.Button('DataMake', key='-DataMake-')]]
-
-NetMake = [
-    [sg.Text('層の種類'), sg.Text('ユニット数')],
-    [sg.InputCombo(('input', 'Dense'), size=(15, 1)), sg.InputText('50', size=(5, 1))],
-    [sg.Text('重みの初期値')],
-    [sg.InputCombo(('Xavier', 'He'), size=(15, 1))],
-    [sg.Text('活性化関数')],
-    [sg.InputCombo(('relu', 'sigmoid', 'liner'), size=(15, 1))],
-    [sg.Text('損失関数')],
-    [sg.InputCombo(('mean_squared_error',), size=(20, 1), key='-loss-')],
-    [sg.Text('評価関数')],
-    [sg.InputCombo(('r2', 'rmse'), size=(15, 1))],
-    [sg.Button('NetMake', key='-NetMake-')]]
-
-
-NuralNet = [
-    [sg.Text('学習用データ')],
-    [sg.Input(size=(30, 1)), sg.FileBrowse(key='-orgLRN-')],
-    [sg.Text('学習用ラベル')],
-    [sg.Input(size=(30, 1)), sg.FileBrowse(key='-orgTrg-')],
-    [sg.Text('Validation用データ')],
-    [sg.Input(size=(30, 1)), sg.FileBrowse(key='-valRLN-')],
-    [sg.Text('Validation用ラベル')],
-    [sg.Input(size=(30, 1)), sg.FileBrowse(key='-valTrg-')],
-    [sg.Radio('標準化', 'feature', size=(10, 1), key='-feature_0-'),
-     sg.Radio('正規化', 'feature', size=(10, 1), key='-feature_1-'),
-     sg.Radio('両方', 'feature', size=(10, 1),  key='-feature_2-')],
-    [sg.Text('Batch Size'), sg.Text('epochs')],
-    [sg.InputText('128', size=(10, 1), key='-Batch-'), sg.InputText('100', size=(10, 1), key='-epochs-')],
-    [sg.Button('Training', key='-TrainingRUN-')],
-    [sg.Text('テスト用データ')],
-    [sg.Input(size=(30, 1)), sg.FileBrowse(key='-tstRLN-')],
-    [sg.Text('テスト用ラベル')],
-    [sg.Input(size=(30, 1)), sg.FileBrowse(key='-tstTrg-')],
-    ]
 
 layout = [
     [sg.Text('Dobotを接続する')], 
@@ -217,10 +115,8 @@ layout = [
     [sg.Frame('Save', 
         [[sg.Column(saveOrg)],
          [sg.Column(saveVal)],
-        ]),
-     sg.Frame('NetMake', NetMake)],
-    [sg.Frame('DataConv', dataConv),
-     sg.Frame('NuralNet', NuralNet),],
+        ])
+    ],
     [sg.Quit()],
 ]
 
@@ -247,31 +143,3 @@ while True:
             print('Dobotに接続していません。')
         else:
             SaveValidation_click(CON_STR, values['-valSave-'])
-    elif event is '-DataMake-':
-        DataMake_click(values['-orgData-'], values['-lrnData-'], values['-tstData-'], values['-Digit-'])
-    elif event is '-NetMake-':
-        NetMake_click(values['-loss-'])
-    # Trainig
-    elif event is '-TrainingRUN-':
-        #Training_click((values['-orgLRN-'], values['-orgTrg-']), values['Batch'], values['epochs'], feature, 
-        #----------------------------
-        # ラジオボタンの条件分岐
-        #----------------------------
-        if values['-feature_0-'] is True:
-            feature = 0
-        elif values['-feature_1-'] is True:
-            feature = 1
-        elif values['-feature_2-'] is True:
-            feature = 2
-        else:
-            feature = None
-        #----------------------------
-        # Validationの条件分岐
-        #----------------------------
-        if (values['-valRLN-'] != '' and values['-valTrg-'] != ''):
-            val = (values['-valRLN-'], values['-valTrg-'])
-        else:
-            val = None
-
-        Training_click((values['-orgLRN-'], values['-orgTrg-']), values['-Batch-'], values['-epochs-'], feature, val)
-        #print(event, values)
