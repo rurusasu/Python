ecf73e7284a9b0d7c61b6de95dda3f089e78decd
rurusasu <36875744+rurusasu@users.noreply.github.com>
2019-11-08 14:28:24 +0900

    xlsxデータを変更

--- Robot/Demo_1103/アプリケーション_2.py
+++ Robot/Demo_1103/アプリケーション_2.py
@@ -8,7 +8,7 @@ from ctypes import * # cdllを呼ぶために必要
 import tkinter as tk
 from tkinter import messagebox as mbox
 from tkinter import Checkbutton as cbutton
-from common.csvIO import csvIO
+from common.csvIO import*
 from nn import nn
 from common.DobotFunction import*
 
