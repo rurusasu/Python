8884964b152807d52b1eee518c0722fdd5ccfb77
F116056 <F116056@ed.socu.ac.jp>
2020-01-27 19:55:56 +0900

    重心を計算する機能を追加

--- 画像認識/画像処理/Threshold_Test_0126.py
+++ 画像認識/画像処理/Threshold_Test_0126.py
@@ -72,10 +72,34 @@ def adaptive_binalize(src, method=cv2.ADAPTIVE_THRESH_MEAN_C, Type=cv2.THRESH_BI
     return dst
 
 def Twothresh_binalize(src, LowerThreshold=0, UpperThreshold=128, PickupColor=4, Type=cv2.THRESH_BINARY):
-    dst = img = src.copy()
-    dst[img < LowerThreshold] = 0
-    dst[img > UpperThreshold] = 255
+    r, g, b = cv2.split(src)
+
+    # for Red
+    _, IMAGE_R_bw = binalize(r, LowerThreshold, Type)
+    _, IMAGE_R__ = binalize(r, UpperThreshold, Type)
+    IMAGE_R__ = cv2.bitwise_not(IMAGE_R__)
+    # for Green
+    _, IMAGE_G_bw = binalize(g, LowerThreshold, Type)
+    _, IMAGE_G__ = binalize(g, UpperThreshold, Type)
+    IMAGE_G__ = cv2.bitwise_not(IMAGE_G__)
+    # for Blue
+    _, IMAGE_B_bw = binalize(b, LowerThreshold, Type)
+    _, IMAGE_B__ = binalize(b, UpperThreshold, Type)
+    IMAGE_B__ = cv2.bitwise_not(IMAGE_B__)
+
+    if PickupColor == 0:
+        dst = IMAGE_R_bw*IMAGE_G__*IMAGE_B__   # 画素毎の積を計算　⇒　赤色部分の抽出
+    elif PickupColor == 1:
+        dst = IMAGE_G_bw*IMAGE_B__*IMAGE_R__   # 画素毎の積を計算　⇒　緑色部分の抽出
+    elif PickupColor == 2:
+        dst = IMAGE_B_bw*IMAGE_R__*IMAGE_G__   # 画素毎の積を計算　⇒　青色部分の抽出
+    elif PickupColor == 3:
+        dst = IMAGE_R_bw*IMAGE_G_bw*IMAGE_B_bw  # 画素毎の積を計算　⇒　白色部分の抽出
+    elif PickupColor == 4:
+        dst = IMAGE_R__*IMAGE_G__*IMAGE_B__    # 画素毎の積を計算　⇒　黒色部分の抽出
+
     ret_low, ret_up = LowerThreshold, UpperThreshold
+
     return (ret_low, ret_up), dst
 
 
@@ -130,13 +154,82 @@ def Image_hist(img, ax, ticks=None, thresh=None):
     if ticks:
         ax.set_xticks(ticks)
     if thresh:
-        #ax.vlines(thresh, 0, hist.max(), "blue", linestyles='dashed')
-        for i in thresh:
-            ax.vlines(i, 0, hist.max(), "blue", linestyles='dashed')
+        if type(thresh) is tuple:
+            for i in thresh:
+                ax.vlines(i, 0, hist.max(), "blue", linestyles='dashed')
+        else:
+            ax.vlines(thresh, 0, hist.max(), "blue", linestyles='dashed')
     ax.set_title('histogram')
     ax.set_xlim([0, 256])
 
     return ax
+
+
+#---------------------------------
+# 重心位置を計算するための関数
+#---------------------------------
+def CenterOfGravity(src, cal_Method=0):
+    """
+    オブジェクトの重心を計算する関数
+
+    Parameter
+    ---------
+    org_img : OpenCV型
+        重心計算方法用の画像
+    contours : OpenCV型
+        画像から抽出した輪郭情報
+    cal_Method : int
+        重心計算を行う方法を選択する
+        0: 画像から重心を計算
+        1: オブジェクトの輪郭から重心を計算
+
+    Returns
+    -------
+    cx, cy : int
+        オブジェクトの重心座標
+    """
+    new_img = src.copy()
+    # 画像をもとに重心を求める場合
+    if cal_Method == 0:  
+        M = cv2.moments(new_img, False)
+    
+    # 輪郭から重心を求める場合
+    else:
+        new_img, contours, hierarchy = cv2.findContours(new_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        maxCont = contours[0]
+
+        for c in contours:
+            if len(maxCont)< len(c):
+                maxCont = c
+        
+        M = cv2.moments(maxCont)
+    if int(M['m00']) == 0:
+        return None
+    try:
+        cx, cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+    except ZeroDivisionError:
+        return None
+
+    return (cx, cy), new_img
+
+
+def drawing_edge(src, contours, min_area):
+    """
+    入力されたimgに抽出した輪郭線を描く関数
+
+    Parameters
+    ----------
+    img : OpenCV型
+        輪郭線を描く元データ
+    contours : OpenCV型
+        画像から抽出した輪郭情報
+    min_area : int
+        領域が占める面積の閾値を指定
+    """
+    large_contours = [
+        cnt for cnt in contours if cv2.contourArea(cnt) > min_area]
+    
+    cv2.drawContours(src, large_contours, -1, color=(0, 255, 0), thickness=1)
 
 
 #---------------------------------------------
@@ -189,11 +282,18 @@ WebCam = [
                     'Otsu',
                     'Adaptive',
                     'Bradley',
-                    #'Two_Thresh'
-                    'NPR'), size=(6, 1), key='-Binary_Type-', readonly=True),],
-    [sg.Input(size=(30, 1), disabled=True), sg.FileBrowse(key='-Image_path-'), ],
-    [sg.Image(filename='', size=(Image_width, Image_height), key='image')],
-    [sg.Image(filename='', size=(Image_width, Image_height), key='image_2')]
+                    'Two_Thresh',), size=(6, 1), key='-Binary_Type-', readonly=True),],
+    [sg.Input(size=(30, 1), disabled=True), sg.FileBrowse(key='-Image_path-'),], 
+    [sg.Text('抽出する色', size=(22, 1), background_color='grey63', justification='center'), 
+     sg.Text('重心の計算方法', size=(17, 1), background_color='grey63', justification='center')],
+    [sg.Radio('R', group_id='color', background_color='grey59', text_color='red', key='-color_R-', pad=(0, 0)),
+     sg.Radio('G', group_id='color', background_color='grey59', text_color='green', key='-color_G-', pad=(0, 0)),
+     sg.Radio('B', group_id='color', background_color='grey59', text_color='blue', key='-color_B-', pad=(0, 0)),
+     sg.Radio('W', group_id='color', background_color='grey59', text_color='snow', key='-color_W-', pad=(0, 0)),
+     sg.Radio('Bk', group_id='color', default=True, background_color='grey59', text_color='grey1', key='-color_Bk-', pad=(0, 0)),
+     sg.InputCombo(('なし',
+                    'Image',
+                    '輪郭をもとに計算', ), size=(17, 1), key='-CenterOfGravity-', readonly=True)],
 ]
 
 slider = [
@@ -201,11 +301,20 @@ slider = [
      sg.Slider((0, 255), default_value=126, resolution=1, orientation='horizontal', size=(28, 5), key='-LowerThresh-')],
     [sg.Text('Upper'),
      sg.Slider((0, 255), default_value=126, resolution=1, orientation='horizontal', size=(28, 5), key='-UpperThresh-')],
-    [sg.Canvas(size=(Image_width, Image_height), key='-CANVAS-')], ]
+    ]
+
+image = [
+    [sg.Image(filename='', size=(Image_width, Image_height), key='image')],
+    [sg.Image(filename='', size=(Image_width, Image_height), key='image_2')],
+]
+
+canvas = [
+    [sg.Canvas(size=(Image_width, Image_height), key='-CANVAS_1-')], 
+]
 
 layout = [
-    [sg.Col(WebCam, background_color='grey63'),
-     sg.Col(slider, background_color='grey63')],]
+    [sg.Col(WebCam, background_color='grey63'), sg.Col(slider, background_color='grey63'),],
+    [sg.Col(image, background_color='grey63'), sg.Col(canvas, background_color='grey63')],]
 
 
 window = sg.Window('Demo Application - OpenCV Integration', layout, location=(800,400))
@@ -240,23 +349,36 @@ while True:                     # The PSG "Event Loop"
         dst = adaptive_binalize(src_2)
         #elif values['-Binary_Type-'] is 'Bradley':
     elif values['-Binary_Type-'] is 'Two_Thresh':
-        ret, dst = Twothresh_binalize(src_2, LowerThreshold=values['-LowerThresh-'], UpperThreshold=values['-UpperThresh-'])
+        if values['-color_R-']: pickup = 0
+        elif values['-color_G-']: pickup = 1
+        elif values['-color_B-']: pickup = 2
+        elif values['-color_W-']: pickup = 3
+        elif values['-color_Bk-']: pickup = 4
+        ret, dst = Twothresh_binalize(src_1, LowerThreshold=values['-LowerThresh-'], UpperThreshold=values['-UpperThresh-'], PickupColor=pickup)
 
 
     #------------------------------------------------
     # 画面上に撮影した画像のヒストグラムを表示する
     #------------------------------------------------
-    canvas_elem_1 = window['-CANVAS-']
+    canvas_elem_1 = window['-CANVAS_1-']
     canvas_1 = canvas_elem_1.TKCanvas
     fig_1, ax_1 = plt.subplots()
-    ax_1 = Image_hist(src_2, ax_1, ticks, ret)
+    #ax_1 = Image_hist(src_2, ax_1, ticks, ret)
+    ax_1 = Image_hist(src_1, ax_1, ticks, ret)
 
     if fig_agg_1: delete_figure_agg(fig_agg_1)
 
     fig_agg_1 = draw_figure(canvas_1, fig_1)
     fig_agg_1.draw()
+    
+    
+    if values['-CenterOfGravity-'] is 'なし': pass
+    elif values['-CenterOfGravity-'] is 'Image':
+        (Gx, Gy), dst = CenterOfGravity(dst, 0)
+        cv2.drawMarker(dst, (Gx, Gy), (0, 255, 0), markerType=cv2.MARKER_TILTED_CROSS, markerSize=15)
+    elif values['-CenterOfGravity-'] is '輪郭をもとに計算':
+        (Gx, Gy), dst = CenterOfGravity(dst, 1)
+        cv2.drawMarker(dst, (Gx, Gy), (0, 255, 0), markerType=cv2.MARKER_TILTED_CROSS, markerSize=15)
 
-    print(src_2)
-
-    window['image'].update(data=cv2.imencode('.png', src_2)[1].tobytes()) # Update image in window
+    window['image'].update(data=cv2.imencode('.png', src_1)[1].tobytes()) # Update image in window
     window['image_2'].update(data=cv2.imencode('.png', dst)[1].tobytes()) # Update image in window
