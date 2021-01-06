6f3860fae5dbd092d8e25139ce279c8cc5290cb7
rurusasu <36875744+rurusasu@users.noreply.github.com>
2021-01-06 11:48:12 +0900

    データを更新

--- AI/Analysis/src/config/config.py
+++ AI/Analysis/src/config/config.py
@@ -1,6 +1,7 @@
-from easydict import EasyDict
 import os
 import sys
+
+from easydict import EasyDict
 
 cfg = EasyDict()
 
@@ -19,6 +20,36 @@ cfg.EVAL_INDEXES_DIR = os.path.join(cfg.SRC_DIR, 'eval_indexes')
 DataDir
 """
 cfg.TEST_DIR = os.path.join(cfg.DATA_DIR, 'test')
+
+"""
+Angledetection
+"""
+cfg.ANGLE_DETECTION_DIR = os.path.join(cfg.DATA_DIR, 'AngleDetection')
+#################
+# DeBug用
+#################
+cfg.DEBUG_DIR = os.path.join(cfg.ANGLE_DETECTION_DIR, 'DeBug')
+cfg.DEBUG_ONEIMAGE_DIR = os.path.join(cfg.DEBUG_DIR, 'OneImage')
+#################
+# OrigData
+#################
+cfg.ANGLE_ORIGINAL_DIR = os.path.join(cfg.ANGLE_DETECTION_DIR, 'Original')
+
+#################
+# TrainingData_1
+#################
+cfg.ANGLE_TRAININGDATA1_DIR = os.path.join(
+    cfg.ANGLE_DETECTION_DIR, 'TrainingData_1')
+cfg.ANGLE_TRAININGDATA1_TRAINING_DIR = os.path.join(
+    cfg.ANGLE_TRAININGDATA1_DIR, 'training')
+cfg.ANGLE_TRAININGDATA1_VALIDATION_DIR = os.path.join(
+    cfg.ANGLE_TRAININGDATA1_DIR, 'validation')
+
+#################
+# TrainingData_2
+#################
+cfg.ANGLE_TRAININGDATA2_DIR = os.path.join(
+    cfg.ANGLE_DETECTION_DIR, 'TrainingData_2')
 
 
 def add_path():
