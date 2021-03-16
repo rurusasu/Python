23bf1aa20a4bfef71b533669b34d9a883f556997
rurusasu <36875744+rurusasu@users.noreply.github.com>
2021-03-16 16:43:36 +0900

    アプリケーションを同期

--- AI/Analysis/gui/ImageProcessing/CenterOfGravity.py
+++ AI/Analysis/gui/ImageProcessing/CenterOfGravity.py
@@ -1,19 +1,17 @@
-import sys, os
+from ImageProcessing.binarization import GlobalThreshold
+from ImageProcessing.GrayScale import AutoGrayScale
+import numpy as np
+import cv2
+import sys
+import os
 
 sys.path.append(".")
 sys.path.append("../")
 sys.path.append("../../")
 
-import cv2
-import numpy as np
-
-from ImageProcessing.GrayScale import AutoGrayScale
-from ImageProcessing.binarization import GlobalThreshold
-
 
 def ExtractContours(
-    org_img,
-    clearly: bool = False,
+    bin_img,
     kernelShape=cv2.MORPH_RECT,
     RetrievalMode=cv2.RETR_LIST,
     ApproximateMode=cv2.CHAIN_APPROX_SIMPLE,
@@ -24,12 +22,8 @@ def ExtractContours(
     黒い背景（暗い色）から白い物体（明るい色）の輪郭を検出すると仮定。
 
     Args:
-        org_img (np.ndarray):
-            変換前の画像データ(二値)
-        clearly (bool optional):
-            ガウシアンフィルタを用いて入力画像のノイズを除去する．
-            * True: 適用する
-            * False: 適用しない: default
+        bin_img (np.ndarray):
+            二値画像
         kernelShape
             モルフォロジー変換で使用する入力画像と処理の性質を決める構造的要素
             カーネルの種類
@@ -60,7 +54,7 @@ def ExtractContours(
         # 画像のチャンネル数が2より大きい場合 ⇒ グレー画像に変換
         img_bin = AutoGrayScale(img_bin, clearly=clearly)
         # 二値化処理
-        img_bin = GlobalThreshold(img_bin,clearly=False)
+        img_bin = GlobalThreshold(img_bin, clearly=False)
 
         """
         モルフォロジー変換
@@ -77,14 +71,15 @@ def ExtractContours(
         # 二値化処理
 
         # 輪郭検出（Detection contours）
-        # tmp_img, contours, _ = cv2.findContours(img_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        tmp_img, contours, _ = cv2.findContours(
+            img_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
         # 輪郭近似（Contour approximation）
-        # approx = approx_contour(contours)
+        approx = approx_contour(contours)
 
         # 等高線の描画（Contour line drawing）
-        # cp_org_img_for_draw = np.copy(org_img)
-        # drawing_edge(cp_org_img_for_draw, approx, min_area)
+        cp_org_img_for_draw = np.copy(org_img)
+        drawing_edge(cp_org_img_for_draw, approx, min_area)
 
         # return contours, cp_org_img_for_draw
         # return contours, tmp_img
@@ -108,7 +103,8 @@ def approx_contour(contours):
     approx = []
     for i in range(len(contours)):
         cnt = contours[i]
-        epsilon = 0.001 * cv2.arcLength(cnt, True)  # 実際の輪郭と近似輪郭の最大距離を表し、近似の精度を表すパラメータ
+        # 実際の輪郭と近似輪郭の最大距離を表し、近似の精度を表すパラメータ
+        epsilon = 0.001 * cv2.arcLength(cnt, True)
         approx.append(cv2.approxPolyDP(cnt, epsilon, True))
     return approx
 
