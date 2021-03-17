1f3c4faca997a991a333bf7110f0318832366e0c
rurusasu <36875744+rurusasu@users.noreply.github.com>
2021-03-17 15:59:06 +0900

    重心計算部分を少し変更

--- AI/Analysis/gui/ImageProcessing/CenterOfGravity.py
+++ AI/Analysis/gui/ImageProcessing/CenterOfGravity.py
@@ -1,13 +1,12 @@
-from ImageProcessing.binarization import GlobalThreshold
-from ImageProcessing.GrayScale import AutoGrayScale
-import numpy as np
-import cv2
 import sys
 import os
 
 sys.path.append(".")
 sys.path.append("../")
 sys.path.append("../../")
+
+import cv2
+import numpy as np
 
 
 def ExtractContours(
@@ -48,14 +47,9 @@ def ExtractContours(
     Returns:
 
     """
-    # 画像が入力されている場合
-    if type(org_img) is np.ndarray:
-        img_bin = org_img.copy()
-        # 画像のチャンネル数が2より大きい場合 ⇒ グレー画像に変換
-        img_bin = AutoGrayScale(img_bin, clearly=clearly)
-        # 二値化処理
-        img_bin = GlobalThreshold(img_bin, clearly=False)
-
+    # 入力が2値画像の場合
+    if (type(bin_img) is np.ndarray) and (len(bin_img.shape) == 2):
+        dst = bin_img.copy()
         """
         モルフォロジー変換
         主に二値画像を対象とし、画像上に写っている図形に対して作用するシンプルな処理．
@@ -66,26 +60,20 @@ def ExtractContours(
         # フィルタの設定
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
         # クロージング処理
-        img_bin = cv2.morphologyEx(img_bin, cv2.MORPH_CLOSE, kernel)
-
-        # 二値化処理
-
+        dst = cv2.morphologyEx(dst, cv2.MORPH_CLOSE, kernel)
         # 輪郭検出（Detection contours）
-        tmp_img, contours, _ = cv2.findContours(
-            img_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
+        contours, hierarchy = cv2.findContours(dst, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
         # 輪郭近似（Contour approximation）
         approx = approx_contour(contours)
-
         # 等高線の描画（Contour line drawing）
-        cp_org_img_for_draw = np.copy(org_img)
-        drawing_edge(cp_org_img_for_draw, approx, min_area)
+        cp_org_img_for_draw = np.copy(dst)
+        dst = drawing_edge(cp_org_img_for_draw, approx, min_area)
 
         # return contours, cp_org_img_for_draw
         # return contours, tmp_img
-        return img_bin
+        return dst
     else:
-        return None, None
+        return None
 
 
 def approx_contour(contours):
@@ -107,6 +95,28 @@ def approx_contour(contours):
         epsilon = 0.001 * cv2.arcLength(cnt, True)
         approx.append(cv2.approxPolyDP(cnt, epsilon, True))
     return approx
+
+
+def drawing_edge(src: np.ndarray, contours: list, min_area: int=10):
+    """
+    入力されたimgに抽出した輪郭線を描く関数
+
+    Args:
+        img (np.ndarray):
+            輪郭線を描く元の画像データ
+        contours (list):
+            画像から抽出した輪郭情報
+        min_area (int)
+            領域が占める面積の閾値を指定
+    Return:
+        dst (np.ndarray):
+            輪郭情報を描画した画像
+    """
+    large_contours = [
+        cnt for cnt in contours if cv2.contourArea(cnt) > min_area]
+
+    dst = cv2.drawContours(img, large_contours, -1, color=(0, 255, 0), thickness=1)
+    return dst
 
 
 def CenterOfGravity(org_img: np.ndarray, contours, cal_Method=0):
@@ -167,15 +177,19 @@ def CenterOfGravity(org_img: np.ndarray, contours, cal_Method=0):
 if __name__ == "__main__":
     from PIL import Image
     from matplotlib import pyplot as plt
+
     from src.config.config import cfg
+    from ImageProcessing.GrayScale import AutoGrayScale
+    from ImageProcessing.Binarization import GlobalThreshold
 
-    img_path = cfg.TEST_DIR + os.sep + "lena.png"
-    img = np.array(Image.open(img_path))
-
+    img_path = cfg.TEST_IMG_ORG_DIR + os.sep + "lena.png"
+    img = np.array(Image.open(img_path)) # ロード
+    img = AutoGrayScale(img, clearly=True) # RGB -> Gray
+    img = GlobalThreshold(img)
     # ----- #
     # テスト #
     # ----  #
     dst = ExtractContours(img)
 
-    plt.imshow(dst)
+    plt.imshow(dst, cmap = "gray")
     plt.show()
