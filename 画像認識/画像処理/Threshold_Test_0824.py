5233d003ed33f575034b7afb5f8ed92f55b4eecf
rurusasu <rurusasu153@gmail.com>
2021-08-24 13:26:41 +0900

    データを同期

--- 画像認識/画像処理/Threshold_Test_0824.py
+++ 画像認識/画像処理/Threshold_Test_0824.py
@@ -20,138 +20,6 @@ ticks = [0, 40, 80, 120, 160, 200, 240]
 binary_color_num = {"r": 0, "g": 1, "b": 2, "w": 3, "bk": 4}
 
 
-class make_bin_img(object):
-    img_elem = {
-        "orig": np.array([]),
-        "gray": np.array([]),
-        "r": np.array([]),
-        "g": np.array([]),
-        "b": np.array([]),
-    }
-    bin_elem = {
-        "r": np.array([]),
-        "g": np.array([]),
-        "b": np.array([]),
-        "w": np.array([]),
-        "bk": np.array([]),
-    }
-    bin_elem_rev = {
-        "r_rev": np.array([]),
-        "g_rev": np.array([]),
-        "b_rev": np.array([]),
-    }
-    lower_thred = 0
-    upper_thred = 0
-
-    def __init__(self, src: np.ndarray, threshold: Type[Union[int, tuple]]) -> None:
-        self.img_elem["orig"] = src
-        if len(src.shape) == 1:
-            self.img_elem["gray"] = src
-        elif len(src.shape) == 3:
-            self.img_elem["gray"] = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
-            [self.img_elem["r"], self.img_elem["g"], self.img_elem["b"]] = Disassembly(
-                src
-            )
-
-        # 1つの閾値しか入力されていない場合
-        if type(threshold) == int and (0 <= threshold <= 255):
-            self.lower_thred = threshold
-        # 複数の閾値が設定されている場合
-        elif type(threshold) == tuple and (0 <= min(threshold), max(threshold) <= 255):
-            self.lower_thred = min(threshold)
-            self.upper_thred = max(threshold)
-        else:
-            raise ValueError("Input value is invalid.")
-
-    def binarize(self, binary_type: str):
-        # 2つの閾値を用いる場合とそうでない処理で条件分岐
-        # なぜなら、2つの閾値を用いる場合は最大2個、そうでない処理の場合は最大3個の独立した閾値を持つため。
-        if values["-Binary_Type-"] == "Two_Thresh":
-            self._two_thresh_binalize(
-                LowerThreshold=self.lower_thred,
-                UpperThreshold=self.upper_thred,
-            )
-        else:
-            for i, color in enumerate(self.img_elem):
-                if color == "orig" or color == "gray":
-                    continue
-
-                if binary_type == "Global":
-                    ret, dst = binalize(self.img_elem[color], self.lower_thred)
-                elif binary_type == "Otsu":
-                    ret, dst = otsu_binalize(self.img_elem[color])
-                elif binary_type == "Adaptive":
-                    dst = adaptive_binalize(self.img_elem[color])
-
-                # bit 反転
-                dst_rev = cv2.bitwise_not(dst)
-                self.bin_elem[color], self.bin_elem_rev[f"{color}_rev"] = dst, dst_rev
-
-            self._w_and_bk_binarize()
-
-        return self.img_elem, self.bin_elem
-
-    def _w_and_bk_binarize(self) -> None:
-        # White color pickup
-        w = self.bin_elem["r"] * self.bin_elem["g"] * self.bin_elem["b"]
-        self.bin_elem["w"] = w
-
-        # bk color pickup
-        bk = (
-            self.bin_elem_rev["r_rev"]
-            * self.bin_elem_rev["g_rev"]
-            * self.bin_elem_rev["b_rev"]
-        )
-        self.bin_elem["bk"] = bk
-
-    def _two_thresh_binalize(
-        self,
-        LowerThreshold: int = 0,
-        UpperThreshold: int = 128,
-        Type=cv2.THRESH_BINARY,
-    ) -> None:
-        """2つの閾値を用いて画像を2値化処理する関数
-        Args:
-            LowerThreshold (int, optional): 低い閾値. Defaults to 0.
-            UpperThreshold (int, optional): 高い閾値. Defaults to 128.
-            Type ([type], optional): 2値化処理のタイプ. Defaults to cv2.THRESH_BINARY.
-        """
-        color_list = ["r", "g", "b"]
-
-        for c in color_list:
-            _, self.bin_elem[c] = binalize(self.img_elem[c], LowerThreshold, Type)
-            _, tmp = binalize(self.img_elem["r"], UpperThreshold, Type)
-            self.bin_elem_rev[f"{c}_rev"] = cv2.bitwise_not(tmp)
-
-        # 画素毎の積を計算　⇒　赤色部分の抽出
-        dst_r = (
-            self.bin_elem["r"] * self.bin_elem_rev["g_rev"] * self.bin_elem_rev["b_rev"]
-        )
-        # 画素毎の積を計算　⇒　緑色部分の抽出
-        dst_g = (
-            self.bin_elem_rev["r_rev"] * self.bin_elem["g"] * self.bin_elem_rev["b_rev"]
-        )
-        # 画素毎の積を計算　⇒　青色部分の抽出
-        dst_b = (
-            self.bin_elem_rev["r_rev"] * self.bin_elem_rev["g_rev"] * self.bin_elem["b"]
-        )
-        # 画素毎の積を計算　⇒　白色部分の抽出
-        self.bin_elem["w"] = (
-            self.bin_elem["r"] * self.bin_elem["g"] * self.bin_elem["b"]
-        )
-        # 画素毎の積を計算　⇒　黒色部分の抽出
-        self.bin_elem["bk"] = (
-            self.bin_elem_rev["r_rev"]
-            * self.bin_elem_rev["g_rev"]
-            * self.bin_elem_rev["b_rev"]
-        )
-
-        # ほかの色も保存
-        self.bin_elem["r"] = dst_r
-        self.bin_elem["g"] = dst_g
-        self.bin_elem["b"] = dst_b
-
-
 # ------------------------------
 # 空間フィルタリングを行う関数
 # ------------------------------
@@ -458,7 +326,7 @@ slider = [
         ),
         sg.Slider(
             (0, 255),
-            default_value=80,
+            default_value=160,
             resolution=1,
             orientation="horizontal",
             size=(28, 5),
@@ -503,6 +371,7 @@ image = [
         sg.Image(filename="", size=(Image_width, Image_height), key="-Image_b-"),
     ],
     [
+        sg.Image(filename="", size=(Image_width, Image_height), key="-Image_gray_bin-"),
         sg.Image(filename="", size=(Image_width, Image_height), key="-Image_r_bin-"),
         sg.Image(filename="", size=(Image_width, Image_height), key="-Image_g_bin-"),
         sg.Image(filename="", size=(Image_width, Image_height), key="-Image_b_bin-"),
@@ -572,6 +441,7 @@ while True:  # The PSG "Event Loop"
             # 画像をリサイズ
             src = scale_box(src, Image_width, Image_height)
 
+            # <<< 二値化処理 <<<
             mk_bin = make_bin_img(
                 src, threshold=(values["-LowerThresh-"], values["-UpperThresh-"])
             )
--- 画像認識/画像処理/lib/utils/binarization.py
+++ 画像認識/画像処理/lib/utils/binarization.py
@@ -1,5 +1,145 @@
 import cv2
+from typing import Type, Union
+
 import numpy as np
+
+from lib.utils.base_utils import Disassembly
+
+
+class make_bin_img(object):
+    img_elem = {
+        "orig": np.array([]),
+        "gray": np.array([]),
+        "r": np.array([]),
+        "g": np.array([]),
+        "b": np.array([]),
+    }
+    bin_elem = {
+        "gray": np.array([]),
+        "r": np.array([]),
+        "g": np.array([]),
+        "b": np.array([]),
+        "w": np.array([]),
+        "bk": np.array([]),
+    }
+    bin_elem_rev = {
+        "r_rev": np.array([]),
+        "g_rev": np.array([]),
+        "b_rev": np.array([]),
+    }
+    lower_thred = 0
+    upper_thred = 0
+
+    def __init__(self, src: np.ndarray, threshold: Type[Union[int, tuple]]) -> None:
+        self.img_elem["orig"] = src
+        if len(src.shape) == 1:
+            self.img_elem["gray"] = src
+        elif len(src.shape) == 3:
+            self.img_elem["gray"] = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
+            [self.img_elem["r"], self.img_elem["g"], self.img_elem["b"]] = Disassembly(
+                src
+            )
+
+        # 1つの閾値しか入力されていない場合
+        if type(threshold) == int and (0 <= threshold <= 255):
+            self.lower_thred = threshold
+        # 複数の閾値が設定されている場合
+        elif type(threshold) == tuple and (0 <= min(threshold), max(threshold) <= 255):
+            self.lower_thred = min(threshold)
+            self.upper_thred = max(threshold)
+        else:
+            raise ValueError("Input value is invalid.")
+
+    def binarize(self, binary_type: str):
+        # 2つの閾値を用いる場合とそうでない処理で条件分岐
+        # なぜなら、2つの閾値を用いる場合は最大2個、そうでない処理の場合は最大3個の独立した閾値を持つため。
+        if binary_type == "Two_Thresh":
+            self._two_thresh_binalize(
+                LowerThreshold=self.lower_thred,
+                UpperThreshold=self.upper_thred,
+            )
+        else:
+            for i, color in enumerate(self.img_elem):
+                if color == "orig":
+                    continue
+
+                if binary_type == "Global":
+                    ret, dst = binalize(self.img_elem[color], self.lower_thred)
+                elif binary_type == "Otsu":
+                    ret, dst = otsu_binalize(self.img_elem[color])
+                elif binary_type == "Adaptive":
+                    dst = adaptive_binalize(self.img_elem[color])
+
+                # bit 反転した画像を保存
+                if color != "gray":
+                    dst_rev = cv2.bitwise_not(dst)
+                    self.bin_elem_rev[f"{color}_rev"] = dst_rev
+                self.bin_elem[color] = dst
+
+            self._w_and_bk_binarize()
+
+        return self.img_elem, self.bin_elem
+
+    def _w_and_bk_binarize(self) -> None:
+        # White color pickup
+        w = self.bin_elem["r"] * self.bin_elem["g"] * self.bin_elem["b"]
+        self.bin_elem["w"] = w
+
+        # bk color pickup
+        bk = (
+            self.bin_elem_rev["r_rev"]
+            * self.bin_elem_rev["g_rev"]
+            * self.bin_elem_rev["b_rev"]
+        )
+        self.bin_elem["bk"] = bk
+
+    def _two_thresh_binalize(
+        self,
+        LowerThreshold: int = 0,
+        UpperThreshold: int = 128,
+        Type=cv2.THRESH_BINARY,
+    ) -> None:
+        """2つの閾値を用いて画像を2値化処理する関数
+        Args:
+            LowerThreshold (int, optional): 低い閾値. Defaults to 0.
+            UpperThreshold (int, optional): 高い閾値. Defaults to 128.
+            Type ([type], optional): 2値化処理のタイプ. Defaults to cv2.THRESH_BINARY.
+        """
+        color_list = ["r", "g", "b"]
+
+        for c in color_list:
+            _, self.bin_elem[c] = binalize(self.img_elem[c], LowerThreshold, Type)
+            _, tmp = binalize(self.img_elem["r"], UpperThreshold, Type)
+            self.bin_elem_rev[f"{c}_rev"] = cv2.bitwise_not(tmp)
+
+        # 画素毎の積を計算　⇒　赤色部分の抽出
+        dst_r = (
+            self.bin_elem["r"] * self.bin_elem_rev["g_rev"] * self.bin_elem_rev["b_rev"]
+        )
+        # 画素毎の積を計算　⇒　緑色部分の抽出
+        dst_g = (
+            self.bin_elem_rev["r_rev"] * self.bin_elem["g"] * self.bin_elem_rev["b_rev"]
+        )
+        # 画素毎の積を計算　⇒　青色部分の抽出
+        dst_b = (
+            self.bin_elem_rev["r_rev"] * self.bin_elem_rev["g_rev"] * self.bin_elem["b"]
+        )
+        # 画素毎の積を計算　⇒　白色部分の抽出
+        self.bin_elem["w"] = (
+            self.bin_elem["r"] * self.bin_elem["g"] * self.bin_elem["b"]
+        )
+        # 画素毎の積を計算　⇒　黒色部分の抽出
+        self.bin_elem["bk"] = (
+            self.bin_elem_rev["r_rev"]
+            * self.bin_elem_rev["g_rev"]
+            * self.bin_elem_rev["b_rev"]
+        )
+
+        # ほかの色も保存
+        self.bin_elem["r"] = dst_r
+        self.bin_elem["g"] = dst_g
+        self.bin_elem["b"] = dst_b
+
 
 # ---------------
 # グローバル二値化
